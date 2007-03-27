edc97423f45195bbfebca7c9ee14590639385e63
Simon Arlott <simon.arlott.org.uk>
2007-03-27 17:58:01 +0000

    implement all log types

--- src/console.c
+++ src/console.c
@@ -2,6 +2,30 @@
 
 #include "output.h"
 
-void log_error(const char *type, int code, const char *msg) {
+void log_any(const char *type, int code, const char *msg) {
 	printf("%s[%d]: %s\n", type, code, msg);
+}
+
+void log_fatal(const char *type, int code, const char *msg) {
+	log_any(type, code, msg);
+}
+
+void log_error(const char *type, int code, const char *msg) {
+	log_any(type, code, msg);
+}
+
+void log_warn(const char *type, int code, const char *msg) {
+	log_any(type, code, msg);
+}
+
+void log_notice(const char *type, int code, const char *msg) {
+	log_any(type, code, msg);
+}
+
+void log_info(const char *type, int code, const char *msg) {
+	log_any(type, code, msg);
+}
+
+void log_debug(const char *type, int code, const char *msg) {
+	log_any(type, code, msg);
 }
