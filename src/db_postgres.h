0f64c7723808df4bd1d2a3143751c8b471462e2e
Simon Arlott <simon.arlott.org.uk>
2009-10-29 21:08:07 +0000

    add some psql debug

--- src/db_postgres.h
+++ src/db_postgres.h
@@ -30,3 +30,29 @@ PGconn *conn;
 	} \
 } while(0)
 
+#if 0
+#define PQprepare(conn, name, sql, num, x) (\
+	printf("PQprepare(%s) = %s\n", name, sql) \
+, \
+	PQprepare(conn, name, sql, num, x) \
+)
+
+#define PQexecPrepared(conn, name, num, param, x, y, z) (\
+	({ \
+		int __i; \
+		printf("PQexec(%s)", name); \
+		if (num > 0) { \
+			printf(" { "); \
+			for (__i = 0; __i < num; __i++) { \
+				if (__i > 0) \
+					printf(", "); \
+				printf("%d = '%s'", __i, ((const char **)param)[__i]); \
+			} \
+			printf(" }"); \
+		} \
+		printf("\n"); \
+	}) \
+, \
+	PQexecPrepared(conn, name, num, param, x, y, z) \
+)
+#endif
